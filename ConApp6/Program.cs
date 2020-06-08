79af1a4cc41b22031571f42d61f0afe483fe74e5
staab_edu <staab_edu@outlook.com>
2020-06-08 02:54:42 -0700

    app 6 - BOOLEAN LOGIC

---
+++ ConApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tics = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && !dui && tics <= 3;
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
