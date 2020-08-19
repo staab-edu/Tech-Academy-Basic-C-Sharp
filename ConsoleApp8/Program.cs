ff1112b1ceff93214fbe934bc63adce48c63d53d
staab_edu <staab_edu@outlook.com>
2020-08-19 14:38:41 -0700

    While loops

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l=1;
+            int i;
+            bool lp=true;
+            while(lp)
+            {
+                for (i = 0; i < l; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+                l++;
+                if (l == 5) lp = false;
+            }
+            do
+            {
+                for (i = 0; i < l; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+                l--;
+                if (l == 0) lp = true;
+            } while (!lp);
+            Console.ReadLine();
+        }
+    }
+}
