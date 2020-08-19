651dff6ed978edd3e0fb6686e259963b8c887e6f
staab_edu <staab_edu@outlook.com>
2020-08-19 14:39:42 -0700

    String manipulation

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello ";
+            string str2 = "World! ";
+            string str3 = "Its 2020.";
+            string msg;
+            string umsg;
+            msg = str1 + str2 + str3;
+            Console.WriteLine(msg);
+            umsg = msg.ToUpper();
+            Console.WriteLine(umsg);
+            StringBuilder strb = new StringBuilder();
+            strb.Append("This is a test. ");
+            strb.Append("This paragraph was made with StringBuilder. ");
+            strb.AppendLine("StringBuilder allows a string to change.");
+
+            Console.WriteLine(strb);
+            Console.ReadLine();
+        }
+    }
+}
