ddab278b9d3dea66dc9c9cf13ee8df0ed258d09c
staab_edu <staab_edu@outlook.com>
2020-08-19 14:40:43 -0700

    Simple array/list

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "Red", "Blue", "White", "Green", "Yellow"};
+            int[] primes = { 1, 3, 5, 7, 11, 13 };
+            var Fruit = new List<string>();
+            Fruit.Add("banana");
+            Fruit.Add("grape");
+            Fruit.Add("Strawberry");
+            Fruit.Add("cberry");
+            Fruit.Add("blueberry");
+
+            Console.WriteLine("Please pick a number from 0-4");
+            int ind = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(colors[ind]);
+
+            bool invalid = true;
+            while (invalid)
+            {
+                Console.WriteLine("Please pick a number");
+                ind = Convert.ToInt32(Console.ReadLine());
+
+                if (ind < 0 || ind > primes.Length)
+                {
+                    Console.WriteLine("Invalid index for array.  Pick a number from 0 to 5.");
+                }
+                else
+                {
+                    Console.WriteLine(primes[ind]);
+                    invalid = false;
+                }
+            }
+
+            Console.WriteLine("Please pick a number from 0-4");
+            ind = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fruit[ind]);
+            Console.ReadLine();
+
+        }
+    }
+}
