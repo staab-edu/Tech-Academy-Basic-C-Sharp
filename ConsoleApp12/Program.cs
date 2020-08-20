8b423384747638593189307600a6a591f5629161
staab_edu <staab_edu@outlook.com>
2020-08-20 15:27:38 -0700

    Base before try/catch

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 10, 15, 26, 33, 50, 73, 80 };
+
+            Console.WriteLine("Ender number to devide by");
+            int div = Convert.ToInt32(Console.ReadLine());
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number + " divided by " + div + " = " + number / div);
+            }
+            Console.ReadLine();
+        }
+    }
+}
