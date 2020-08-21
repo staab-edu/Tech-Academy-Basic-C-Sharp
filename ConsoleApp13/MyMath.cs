4aab5312e92fa500e4979f5af97732894a69b182
staab_edu <staab_edu@outlook.com>
2020-08-20 19:21:34 -0700

    3 at methods

---
+++ ConsoleApp13/MyMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class MyMath
+    {
+        public int plus(int num)
+        {
+            num = num + num;
+            return num;
+        }
+
+        public int minus(int num)
+        {
+            num = 100 - num;
+            return num;
+        }
+
+        public int times(int num)
+        {
+            num = num * num;
+            return num;
+        }
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer to manuipiate");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            MyMath math = new MyMath();
+            Console.WriteLine(num + " added to self is " + math.plus(num));
+            Console.WriteLine(num + " subtracted from 100 is " + math.minus(num));
+            Console.WriteLine(num + " times to self is " + math.times(num));
+            Console.ReadLine();
+        }
+    }
+}
