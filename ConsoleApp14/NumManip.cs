c2469e90e8e40b7422b4360815385552e4c94a8e
staab_edu <staab_edu@outlook.com>
2020-08-22 15:39:35 -0700

    Method overload

---
+++ ConsoleApp14/NumManip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class NumManip
+    {
+        public int mymanip(int num)
+        {
+            num = num + 20;
+            return num;
+        }
+        public int mymanip(decimal num)
+        {
+            num = num * 10;
+            return Decimal.ToInt32(num);
+        }
+        public int mymanip(string snum)
+        {
+            int num;
+            num = Convert.ToInt32(snum) - 5;
+            return num;
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumManip math1 = new NumManip();
+            int num = 15;
+            Console.WriteLine(num + " added to 20 is " + math1.mymanip(num));
+
+            NumManip math2 = new NumManip();
+            decimal dnum = 7.5m;
+            Console.WriteLine(dnum + " times 10 is " + math2.mymanip(dnum));
+
+            NumManip math3 = new NumManip();
+            string snum = "25";
+            Console.WriteLine(snum + " minus 5 is " + math3.mymanip(snum));
+
+            Console.ReadLine();
+        }
+    }
+}
